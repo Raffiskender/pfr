23df74f7844fc2742bc681749f9f4ede5ec41cdd
Raffi <raffiskender@gmail.com>
2025-04-07 09:06:35 +0200

    création d'un menu dynamique facilitant la gestion des nouvelles pages.

--- VARS.py
+++ VARS.py
@@ -1,17 +1,23 @@
 import streamlit as st
 
-PAGES = [
-    st.Page("./src/views/home.py",       title='Accueil',        default=True),
-    st.Page("./src/views/cleaning.py",   title='Nettoyage',      url_path="cleaning"),
-    st.Page("./src/views/eda.py",        title='Analyse',        url_path="eda"),
-    st.Page("./src/views/viz.py",        title='Visualisations', url_path="viz"),
-    st.Page("./src/views/conclusion.py", title='Conclusion',    url_path="conclusion")
-]
+MENUS_LABEL_MATCH_ROUTES={
+    'Accueil'       :'home',
+    'Nettoyage'     :"cleaning",
+    'Analyse'       :"eda",
+    'Visualisations':"viz",
+    'Conclusion'    :"conclusion",
+    'Admin'         :"admin",
+    'Login'         :"login",
+    'Signin'        :"signin",
+    'Options'       :"options",
+    'Mon compte'    :"account",
+    'Logout'        :'logout',
+}
 
-MENUS_PAGES_TITLES=[
-    'Accueil',
-    'Nettoyage',
-    'Analyse',
-    'Visualisations',
-    'Conclusion'
-]
+
+PAGES = []
+for key, value in MENUS_LABEL_MATCH_ROUTES.items():
+    if key == "Accueil":
+        PAGES.append(st.Page(f"./src/views/{value}.py", title=key, default=True))
+    else:
+        PAGES.append(st.Page(f"./src/views/{value}.py", title=key, url_path=value))
--- src/views/login.py
+++ src/views/login.py
@@ -1,36 +1,13 @@
 import streamlit as st
-from src.controllers.auth import auth
-from src.controllers.signup import signup
+# from src.controllers.auth import auth
+# from src.controllers.signup import signup
 
-from src.router import redirect
+# from src.router import redirect
 
-def load_view():
+def load():
     st.title('Login')
 
-    st.text_input('Email', '', key='email')
-    st.text_input('Password', '', type='password', key='password', on_change=login)
-    col1, col2 = st.columns([1, 1], gap="small")
-    with col1:
-        st.button('Login', on_click=login)
-    with col2:
-        st.button('Sign up', on_click=sign_up)
-
-def login():
-    res = auth(st.session_state.email, st.session_state.password)
-    if not res:
-        st.text("Wrong Account")
-    else:
-        st.session_state.SESSION_LOGGED = res.logged
-        redirect("home")
-
-    
-def sign_up():
-    res = signup(st.session_state.email, st.session_state.password)
-    if not res:
-        st.text("E-mail already used. Please, log in.")
-    else: 
-        st.text("Sign up in progress...")
-        redirect("home")
+load()
 
 
 
